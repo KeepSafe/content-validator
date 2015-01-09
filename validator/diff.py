407a0814b642e4e2d090632e163b68c33cee699d
ca77y <ca77y@live.com>
2015-01-09 17:01:37 +0100

    diff in one file

--- validator/diff.py
+++ validator/diff.py
@@ -41,6 +41,8 @@ html_diff = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         <tr><td width="50%"><p id="left_html"></p></td><td width="50%"><p id="right_html"></p></td></tr>
         </tbody>
     </table>
+
+    <div id="md_diff"></div>
 </body>
 </html>
 """
@@ -73,18 +75,25 @@ class Diff(object):
     def html_to_file(self, parser, reader, base_path, other_path):
         base_name, _ = os.path.splitext(os.path.basename(base_path))
         other_name, _ = os.path.splitext(os.path.basename(other_path))
+        base_html = self._pretty_html(parser, reader, base_path, 'placeholder')
+        other_html = self._pretty_html(parser, reader, other_path, 'placeholder')
         base_content = parser.parse(reader.read(base_path))
         other_content = parser.parse(reader.read(other_path))
+        base_text = html2text.html2text(base_html)
+        other_text = html2text.html2text(other_html)
         base_soup = BeautifulSoup(base_content)
         other_soup = BeautifulSoup(other_content)
         lang = self._get_lang(base_path, other_path)
         output_dir = os.path.join(self.output_dir, lang)
 
+        htmldiff = difflib.HtmlDiff(tabsize=4)
+        diff_content = htmldiff.make_table(base_text.splitlines(), other_text.splitlines(), context=True)
         diff_soup = BeautifulSoup(html_diff)
         self._add_content(diff_soup, 'left_path', base_path)
         self._add_content(diff_soup, 'right_path', other_path)
         self._add_content(diff_soup, 'left_html', base_soup.body)
         self._add_content(diff_soup, 'right_html', other_soup.body)
+        self._add_content(diff_soup, 'md_diff', BeautifulSoup(diff_content))
 
         diff_content = diff_soup.prettify()
         diff_path = os.path.join(output_dir, '{}.html'.format(other_name))
@@ -103,25 +112,6 @@ class Diff(object):
         os.makedirs(output_dir, exist_ok=True)
         with open(error_path, 'w') as fp:
             fp.write(str(error))
-
-
-    def diff_to_file(self, parser, reader, base_path, other_path):
-        base_html = self._pretty_html(parser, reader, base_path, 'placeholder')
-        other_html = self._pretty_html(parser, reader, other_path, 'placeholder')
-        base_name, _ = os.path.splitext(os.path.basename(base_path))
-        other_name, _ = os.path.splitext(os.path.basename(other_path))
-        lang = self._get_lang(base_path, other_path)
-        output_dir = os.path.join(self.output_dir, lang)
-
-        base_text = html2text.html2text(base_html)
-        other_text = html2text.html2text(other_html)
-        htmldiff = difflib.HtmlDiff(tabsize=4)
-        diff_content = htmldiff.make_file(base_text.splitlines(), other_text.splitlines(), context=True)
-        diff_path = os.path.join(output_dir, '{}.md.html'.format(other_name))
-
-        os.makedirs(output_dir, exist_ok=True)
-        with open(diff_path, 'w') as fp:
-            fp.write(diff_content)
 
 def diff(output_dir):
     return Diff(output_dir)
