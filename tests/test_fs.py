f514fc9ddf102ae7b0d3a5de59e105c0e42a76b7
ca77y <ca77y@live.com>
2016-11-04 17:47:01 +0100

    fix tests

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -14,28 +14,32 @@ class TestPaths(TestCase):
         self.assertEqual(expected, actual)
 
     def test_wildcard(self):
-        expected = [[Path('tests/fixtures/flat/test.en.txt'), Path('tests/fixtures/flat/test.fr.txt')]]
+        expected = [[Path('tests/fixtures/flat/test.en.txt'),
+                     Path('tests/fixtures/flat/test.fr.txt')]]
 
         actual = list(files('tests/fixtures/flat/*.txt'))
 
         self.assertEqual(set(*expected), set(*actual))
 
     def test_recursive(self):
-        expected = [[Path('tests/fixtures/lang/de/test1.md'), Path('tests/fixtures/lang/en/test1.md')]]
+        expected = [[Path('tests/fixtures/lang/de/test1.md'),
+                     Path('tests/fixtures/lang/en/test1.md')]]
 
         actual = list(files('tests/fixtures/lang/**/test1.md'))
 
         self.assertEqual(set(*expected), set(*actual))
 
     def test_simple_with_parameter(self):
-        expected = [[Path('tests/fixtures/flat/test.en.txt'), Path('tests/fixtures/flat/test.fr.txt')]]
+        expected = [[Path('tests/fixtures/flat/test.en.txt'),
+                     Path('tests/fixtures/flat/test.fr.txt')]]
 
         actual = list(files('tests/fixtures/flat/test.{lang}.txt', lang='en'))
 
         self.assertEqual(expected, actual)
 
     def test_wildcard_with_parameter(self):
-        expected = [[Path('tests/fixtures/flat/test.en.txt'), Path('tests/fixtures/flat/test.fr.txt')]]
+        expected = [[Path('tests/fixtures/flat/test.en.txt'),
+                     Path('tests/fixtures/flat/test.fr.txt')]]
 
         actual = list(files('tests/fixtures/flat/*.{lang}.txt', lang='en'))
 
@@ -55,14 +59,18 @@ class TestPaths(TestCase):
             ],
         ]
 
-        actual = list(files('tests/fixtures/lang/**/{order}.md', order='test1'))
+        actual = list(
+            files('tests/fixtures/lang/**/{order}.md', order='test1'))
 
-        self.assertEqual(sorted(expected), sorted(actual))
+        self.assertCountEqual(expected[0], actual[0])
+        self.assertCountEqual(expected[1], actual[1])
 
     def test_two_parameters(self):
-        expected = [[Path('tests/fixtures/lang/en/test1.md'), Path('tests/fixtures/lang/de/test1.md')]]
+        expected = [[Path('tests/fixtures/lang/en/test1.md'),
+                     Path('tests/fixtures/lang/de/test1.md')]]
 
-        actual = list(files('tests/fixtures/lang/{lang}/{order}1.md', lang='en', order='test'))
+        actual = list(
+            files('tests/fixtures/lang/{lang}/{order}1.md', lang='en', order='test'))
 
         self.assertEqual(expected, actual)
 
