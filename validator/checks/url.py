1392a2d54d27d3a590216830a73c29c427505345
Lukasz Nosko <sneejkuu@gmail.com>
2017-04-03 18:26:14 +0200

    skip keepsafe:// urls validation emails#23

--- validator/checks/url.py
+++ validator/checks/url.py
@@ -20,7 +20,7 @@ class TextUrlExtractor(object):
     def __init__(self, **kwargs):
         pass
 
-    url_pattern = r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()\[\]<>]' \
+    url_pattern = r'(?i)\b((?:https?://|www\d{0,3}[.]|(!keepsafe://)[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()\[\]<>]' \
         '+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[' \
         '\];:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))'
 
